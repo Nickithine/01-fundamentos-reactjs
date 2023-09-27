83bf30a625f18957ca518a86eb894a81c9bd1c2b
Nickithine <dominique.ithina@gmail.com>
2023-09-27 19:24:24 -0300

    feat: Finalizando estilizações da Sidebar e adicionado nova biblioteca de icones

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,12 +10,18 @@ export function Sidebar() {
       />
       
       <div className={ styles.profile }>
+        <img 
+          className={ styles.avatar }
+          src="https://github.com/Nickithine.png" 
+        />
+
         <strong>Dominique Ithina</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={ 20 } />
           Editar seu perfil
         </a>
       </footer>
